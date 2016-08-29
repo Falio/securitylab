0cb01b831f205bdb4f1073117e700f5d8f2d003a
Falio <f.fremouw@gmail.com>
2016-08-29 12:41:25 +0200

    hex to bytes to base 64

--- cryptochallenges/set1/src/main.java
+++ cryptochallenges/set1/src/main.java
@@ -5,10 +5,21 @@ import java.util.Base64;
  */
 public class main {
 
-    static String string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    static String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
     public static void main(String[] args) {
-        String bytesEncoded = Base64.getEncoder().encodeToString(string.getBytes());
-        System.out.println("ecncoded value is " + new String(bytesEncoded ));
+
+        String bytesEncoded = Base64.getEncoder().encodeToString(hexStringToByteArray(hex));
+        System.out.println("encoded value is " + bytesEncoded);
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
     }
 }
