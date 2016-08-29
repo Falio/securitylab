1ffc514ee0ab35a31c22334866a4e88b16be1332
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2016-08-29 13:04:15 +0200

    challenge 1

--- cryptochallenges/set1/src/main.java
+++ cryptochallenges/set1/src/main.java
@@ -1,13 +1,14 @@
-import java.util.Base64;
+import java.math.BigInteger;
 
 /**
  * Created by Fabian on 29-8-2016.
  */
 public class main {
+
     static String string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
     public static void main(String[] args) {
-        String bytesEncoded = Base64.getEncoder().encodeToString(string.getBytes());
-        System.out.println("encoded value is " + new String(bytesEncoded ));
+        byte[] b = new BigInteger(string,16).toByteArray();
+        System.out.println("string value is " + new String(b ));
     }
 }
