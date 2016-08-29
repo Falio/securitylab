c450a2dba3965c678664a19671b69b4ec3305807
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2016-08-29 12:39:31 +0200

    commit

--- cryptochallenges/set1/src/main.java
+++ cryptochallenges/set1/src/main.java
@@ -4,11 +4,10 @@ import java.util.Base64;
  * Created by Fabian on 29-8-2016.
  */
 public class main {
-
     static String string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
     public static void main(String[] args) {
         String bytesEncoded = Base64.getEncoder().encodeToString(string.getBytes());
-        System.out.println("ecncoded value is " + new String(bytesEncoded ));
+        System.out.println("encoded value is " + new String(bytesEncoded ));
     }
 }
