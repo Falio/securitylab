41231f5a1d2b06f0ad10f97dd6b3f75940d3012f
Falio <f.fremouw@gmail.com>
2016-08-29 13:31:49 +0200

    challenge 2 done

--- cryptochallenges/set1/src/challenge1.java
+++ cryptochallenges/set1/src/challenge1.java
@@ -4,9 +4,9 @@ import java.util.Base64;
 /**
  * Created by Fabian on 29-8-2016.
  */
-public class main {
+public class challenge1 {
 
-    static String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    static private String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
     public static void main(String[] args) {
 
@@ -14,7 +14,7 @@ public class main {
         System.out.println("encoded value is " + bytesEncoded);
     }
 
-    public static byte[] toByteArray(String s) {
+    private static byte[] toByteArray(String s) {
         return DatatypeConverter.parseHexBinary(s);
     }
 }
---
+++ cryptochallenges/set1/src/challenge2.java
@@ -0,0 +1,51 @@
+import javax.xml.bind.DatatypeConverter;
+import java.util.Base64;
+
+/**
+ * Created by Fabian on 29-8-2016.
+ */
+public class challenge2 {
+
+    static private String hex = "1c0111001f010100061a024b53535009181c";
+    static private String hex2 = "686974207468652062756c6c277320657965";
+
+
+    public static void main(String[] args) {
+
+        byte[] array_1 = toByteArray(hex);
+        byte[] array_2 = toByteArray(hex2);
+
+        System.out.println("We got:    " + toHexStr(xOr(array_1, array_2, 17)));
+
+    }
+
+    private static byte[] toByteArray(String s) {
+        return DatatypeConverter.parseHexBinary(s);
+    }
+
+    private static byte[] xOr(byte[] b1, byte[] b2, int len){
+
+        if(len > b1.length || len > b2.length)
+            throw new RuntimeException("Cant do fixed xor longer than buffers");
+        byte[] ret = new byte[len];
+        for(int i=0; i<len; i++)
+        {
+            ret[i] = (byte) (b1[i] ^ b2[i]);
+        }
+        return ret;
+
+    }
+
+    public static String toHexStr(byte[] arr)
+    {
+        String hexKey = "0123456789abcdef";
+
+        String s = "";
+        for (byte b : arr)
+        {
+            s += hexKey.charAt((b >> 4) & 0x0F);
+            s += hexKey.charAt((b >> 0) & 0x0F);
+        }
+        return s;
+    }
+}
